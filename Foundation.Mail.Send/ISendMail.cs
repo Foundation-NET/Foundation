41d06031c9b4ca86cae847584bb93b5afa6bc870
Hallam Smith <me@hcssmith.com>
2022-06-18 10:47:42 +0100

    Remove Foundation.Mail.Send

--- Foundation.Mail.Send/ISendMail.cs
+++
@@ -1,8 +0,0 @@
-namespace Foundation.Mail.Send
-{
-    public interface ISendMail
-    {
-        void SetSmtpServer(string server);
-        void Send(string content, string to, string? cc = null, string? bcc = null);
-    }
-}
